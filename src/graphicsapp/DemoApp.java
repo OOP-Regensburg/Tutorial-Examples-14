d54a46590eefe25409a4e82cc02c40c6470aadfb
Alexander Bazo <alexanderbazo@googlemail.com>
2020-02-04 14:00:57 +0100

    Add description for GraphicsApp

--- src/graphicsapp/DemoApp.java
+++ src/graphicsapp/DemoApp.java
@@ -6,6 +6,13 @@ import de.ur.mi.oop.colors.Colors;
 
 public class DemoApp extends GraphicsApp {
 
+    /**
+     * Implementieren Sie eine GraphicsApp, die es Nutzern und Nutzerinnen erlaubt, über vier unterschiedlich-
+     * farbige, runde Schaltflächen Zufallskreise mit der ausgewählten Farbe zu erstellen. Die Kreise erscheinen
+     * an einer zufälligen Position außerhalb des oberen Bildschirmrandes und fallen mit zufälliger, in jedem Frame
+     * steigender, Geschwindigkeit zu Boden und bleiben dort liegen.
+     */
+
     private static final int CANVAS_WIDTH = 500;
     private static final int CANVAS_HEIGHT = 500;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
